ab7add7d99e147b98f248b24b4504eb7acc16f27
simon19-meet <simon19@meet.mit.edu>
2017-07-30 11:48:41 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -1,9 +1,28 @@
-def draw_1d(n):
-    x="*"
-    f=x*n
-    return f
-star_num= draw_1d(61)
+c1="~"
+c2="!"
+def draw_1d(n,char):
+    return n*char
+star_num= draw_1d(c2,8)
 print(star_num)
+
+for i in range(6):
+    print("*"*10)
+
+def draw_2d(n,m,char):
+    for i in range(n):
+        print(char*m)
+def special_draw_2d(n,m,border,fill):
+    print(border*m)
+    for x in range(n-2):
+        print(border+(fill*(m-2))+border)
+    print(border*m)
+def fib(n):
+    
+
+        
+    
+    
+
 
 
 
